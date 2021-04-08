dbc317a12bdf46427b2956dae3e736cdbb1d34d0
Nikolay Tikhonov <nikolay.tikhonov@emc.com>
2021-04-08 15:57:39 +0300

    OBSDEF-6862

--- tools/build_helper/version_resolver.py
+++ tools/build_helper/version_resolver.py
@@ -14,17 +14,21 @@ artmap = dict()
 
 def update_line(line, marker_text):
     idx = None
+    # determine idx (index string to artmap key)
     if marker_text:
         (_, idx) = marker_text.split(RFW_NEXT_LINE_FLAG, 1)
     else:
         (_, idx) = line.split(RFW_THIS_LINE_FLAG, 1)
 
+    # split incoming line into key, value and comment that should be saved
     left, right = line.split(":", 1)
     rparts = right.split("#", 1)
 
+    # index can point to subkey, if not assume 'version'. then get replacement value
     artid, akey = idx.split(',') if ',' in idx else (idx, 'version')
     new_val = artmap[artid.strip()][akey.strip()]
 
+    # restore comment part if it was present
     rcomment = " #" + rparts[1] if len(rparts) > 1 else '\n'
 
     return "{}: {}{}".format(left, new_val, rcomment)
@@ -42,6 +46,8 @@ def process_yaml(input_yaml, return_modified=False):
 
     oylines = ylines.copy()
     proc_next = None
+
+    # process line by line and support update of 'next' line
     for yi, tx in enumerate(ylines):
         if RFW_THIS_LINE_FLAG in tx or proc_next:
             ylines[yi] = update_line(tx, proc_next)
@@ -53,16 +59,17 @@ def process_yaml(input_yaml, return_modified=False):
         return "".join(ylines)
 
     if oylines != ylines:
-        open(input_yaml + ".proc",'w').write("".join(ylines))
+        open(input_yaml,'w').write("".join(ylines))
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-vs", "--version-slice", help="path to version_slice file", required=True)
+    parser.add_argument("-wd", "--working-dir", help="path to directory, default is ./", default='.')
     args = parser.parse_args()
 
     load_artifacts(args.version_slice)
 
-    for fl in glob.glob('./*/*.yaml', recursive=False):
+    for fl in glob.glob(args.working_dir + '/*/*.yaml', recursive=False):
         process_yaml(fl)
